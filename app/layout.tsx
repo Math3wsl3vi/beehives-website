6711d4b77cd4fcaec335013b236e9eedaa59967b
math3wsl3vi <levimathews9841@gmail.com>
2025-05-15 19:38:13 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/home/Navbar";
+
+import { Toaster } from "@/components/ui/toaster";
+import { AuthProvider } from "@/context/AuthContext";
+
+
+export const metadata: Metadata = {
+  title: "Chakulahub",
+  description: "Meal Booking Platform",
+  manifest: "/manifest.json",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        <AuthProvider>
+        <div>
+          <Navbar/>
+        </div>
+        <Toaster/>
+        {children}
+        <div className="">
+        {/* <BottomBar /> */}
+      </div>
+      </AuthProvider>
+      </body>
+    </html>
+  );
+}
