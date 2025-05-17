3a0ca517221b7673ae18f733fcf8e65ce56a1f16
math3wsl3vi <levimathews9841@gmail.com>
2025-05-17 14:03:44 +0300

    meow

--- components/home/Producttabs.tsx
+++ components/home/Producttabs.tsx
@@ -39,7 +39,7 @@ export default function ProductTabs() {
                 : "border-b-2 border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
             }`}
           >
-            Reviews (24)
+            Reviews 
           </button>
         </nav>
       </div>
